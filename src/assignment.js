df9e44bd9e9e43a442413f4c7e5b58191106f4dc
Ruth Obasi <88226737+ruthietut@users.noreply.github.com>
2022-07-09 08:03:22 +0000

    challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+    for (i = 0; i < arrayOfNumbers.length;  i++) {
+      sum = sum + arrayOfNumbers[i]
+    }
+      return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +31,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function countEvenNumbers(arrayOfNumbers){
+  let evenNumbers = 0;
+  for ( num of arrayOfNumbers){
+    if(num % 2 === 0){
+      evenNumbers++
+    }
+  }
+  return evenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
